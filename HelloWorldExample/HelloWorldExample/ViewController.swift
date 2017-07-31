1c3f9182c1d0b3db2c42ef071cda62f70070bd3a
Okan Bagci <bagciok@gmail.com>
2017-07-31 18:42:12 +0300

    first commit

---
+++ HelloWorldExample/HelloWorldExample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HelloWorldExample
+//
+//  Created by Okan Bagci on 31.07.2017.
+//  Copyright © 2017 Okan Bagci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello World")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ HellooooWorld/HellooooWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HellooooWorld
+//
+//  Created by Okan Bagci on 30.07.2017.
+//  Copyright © 2017 Okan Bagci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
